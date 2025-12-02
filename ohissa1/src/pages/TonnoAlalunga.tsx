de652cb6c2d78210bfdfdb53de1704fce36a4fdb
devconnecteed <dev@connecteed.com>
2025-12-02 17:13:18 +0100

    form

--- ohissa1/src/pages/TonnoAlalunga.tsx
+++ ohissa1/src/pages/TonnoAlalunga.tsx
@@ -46,7 +46,7 @@ const TonnoAlalunga: React.FC = () => {
     return () => window.removeEventListener('resize', onResize);
   }, []);
   
-    const API_BASE = "https://api.ohissa.it:8083";
+    const API_BASE = "https://api.ohissa.it";
   
   // ===== STATO FORM =====
   const [formData, setFormData] = useState({
